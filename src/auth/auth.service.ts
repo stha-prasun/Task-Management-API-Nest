a2c728b83a38644b4793a91f592aeb9aa9865c6e
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-06-26 14:55:48 +0545

    Regsiter

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
+
+    async register(registerDto: RegisterDto) {
+        const existingUser = await this.usersService.findByEmail(registerDto.email);
+
+        if (existingUser) {
+            throw new ConflictException('Email already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+        const user = await this.usersService.createUser({
+            ...registerDto,
+            password: hashedPassword,
+        });
+
+        return {
+            message: 'User registered successfully',
+            user,
+        };
+    }
+}
